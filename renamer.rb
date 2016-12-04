e45da603db958a3882eadd2019da28da63eb0a11
Alex Zirbel <alexzirbel@gmail.com>
2016-12-04 11:54:19 -0800

    Fix movie date reading
    Previously was using EXIF[:create_date] to get the dates movies were
    created. This was wrong for live photos and for videos in general,
    possibly due in part to bad time zone parsing, or possibly due to
    :create_date being the date the video was uploaded/saved. As a result,
    videos were off by a few hours and didn't line up with photos.
    
    Using EXIF[:creation_date] for videos instead seems to fix the problem.

--- renamer.rb
+++ renamer.rb
@@ -43,7 +43,7 @@ class Renamer
     exif_data = Exiftool.new(filepaths)
     filepaths.map do |filepath|
       exif_result = exif_data.result_for(filepath)
-      d_string = exif_result.to_hash[:date_time_original] || exif_result.to_hash[:create_date]
+      d_string = exif_result.to_hash[:date_time_original] || exif_result.to_hash[:creation_date].to_s
       files_with_dates[filepath] = d_string
     end
 
